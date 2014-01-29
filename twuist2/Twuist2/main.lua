de14d502d9783a7d60550150781e8de7068e016a
Tony Morelli <tony@tonymorelli.com>
2014-01-29 14:56:28 -0500

    add quotes

--- twuist2/Twuist2/main.lua
+++ twuist2/Twuist2/main.lua
@@ -8,6 +8,15 @@ function handleAccelerometerChange(event)
 	tx.text = "x: "..event.xGravity;
 	ty.text = "y: "..event.yGravity;
 	tz.text = "z: "..event.zGravity;
+	
+--[[	
+	if ((event.xGravity > .7 and xGravity < .8) and
+	    (event.yGravity > .95 and yGravity < 1.0) and
+	    (event.zGravity > .3 and zGravity < .35)) then
+	    
+			--device is pointing down...    
+	end 
+--]]	
 	if (event.yGravity <0) then
 		system.vibrate()
 	end
