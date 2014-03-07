c389d3756eddde344c7b6cdd846f7a3557100ae7
tmorelli <tony@tonymorelli.com>
2014-03-07 14:02:45 -0500

    Test commit

---
+++ VI-Run/VI-Run/Assets/SceneManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour {
+	public GameObject player1;
+	public GameObject player2;
+	public static float distance = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance = player1.transform.position.z - player2.transform.position.z;
+		Debug.Log(distance);
+	}
+}
