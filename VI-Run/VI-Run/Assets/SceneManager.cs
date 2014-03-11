6c781459fa3f82bd0b0d0ed7e795f7a66547d5fa
tmorelli <tony@tonymorelli.com>
2014-03-11 14:41:52 -0400

    Update to read distance from commands.txt (created by psmove controller)

--- VI-Run/VI-Run/Assets/SceneManager.cs
+++ VI-Run/VI-Run/Assets/SceneManager.cs
@@ -1,5 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Text;
+using System.IO;
+using System;
+
 
 public class SceneManager : MonoBehaviour {
 	public GameObject player1;
@@ -14,5 +18,11 @@ public class SceneManager : MonoBehaviour {
 	void Update () {
 		distance = player1.transform.position.z - player2.transform.position.z;
 		Debug.Log(distance);
+		int rumbleVal = (int)Math.Floor(255.0/Math.Abs(distance));
+		
+		
+		StreamWriter sw = new StreamWriter("commands.txt");
+        sw.WriteLine(rumbleVal);
+		sw.Close();
 	}
 }
